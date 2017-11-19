70b29a90b111d5bfcbe4d1c010fc0c5236e284b7
Mikhail Vaysman <Vaysman@users.noreply.github.com>
2017-11-20 00:54:27 +0100

    update ConverterService tests

--- src/main/java/ru/wiseman/onlinecurrencyconverter/service/ConverterService.java
+++ src/main/java/ru/wiseman/onlinecurrencyconverter/service/ConverterService.java
@@ -1,9 +1,15 @@
 package ru.wiseman.onlinecurrencyconverter.service;
 
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class ConverterService {
+    @NonNull
+    private final ExchangeRateRepository repository;
+
     public OperationDetails r2y(float v) {
         return new OperationDetails("OK", 67.89f);
     }
--- src/test/java/ru/wiseman/onlinecurrencyconverter/service/ConverterServiceTest.java
+++ src/test/java/ru/wiseman/onlinecurrencyconverter/service/ConverterServiceTest.java
@@ -2,24 +2,39 @@ package ru.wiseman.onlinecurrencyconverter.service;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import ru.wiseman.onlinecurrencyconverter.domain.ExchangeRate;
+
+import java.util.Date;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
 
 public class ConverterServiceTest {
+    private static final Date ANY_DATE = new Date();
+
+    @Mock
+    private ExchangeRateRepository repository;
+
+    @InjectMocks
     private ConverterService converterService;
 
     @Before
     public void setUp() throws Exception {
-        converterService = new ConverterService();
+        MockitoAnnotations.initMocks(this);
     }
 
     @Test
     public void y2rWhenValueGraterOrEqualZeroReturnOperationDetails() throws Exception {
-        assertThat(converterService.y2r(333.44f)).isEqualTo(new OperationDetails("OK", 555.66f));
+        given(repository.findTopByOrderByTimestampDesc()).willReturn(new ExchangeRate(0.5296f, ANY_DATE));
+        assertThat(converterService.y2r(333.44f)).isEqualTo(new OperationDetails("OK", 176.59f));
     }
 
     @Test
     public void r2yWhenValueGraterOrEqualZeroReturnOperationDetails() throws Exception {
-        assertThat(converterService.r2y(123.45f)).isEqualTo(new OperationDetails("OK", 67.89f));
+        given(repository.findTopByOrderByTimestampDesc()).willReturn(new ExchangeRate(0.5296f, ANY_DATE));
+        assertThat(converterService.r2y(123.45f)).isEqualTo(new OperationDetails("OK", 233.1f));
     }
 }
