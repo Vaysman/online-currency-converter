f8e00aee444daddaa7948a88b17a6a012062fb00
Mikhail Vaysman <Vaysman@users.noreply.github.com>
2017-11-19 19:29:32 +0100

    add more entity test

--- src/main/java/ru/wiseman/onlinecurrencyconverter/domain/ExchangeRate.java
+++ src/main/java/ru/wiseman/onlinecurrencyconverter/domain/ExchangeRate.java
@@ -1,10 +1,27 @@
 package ru.wiseman.onlinecurrencyconverter.domain;
 
+import lombok.Data;
+import org.springframework.util.Assert;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Date;
 
 @Entity
+@Data
 public class ExchangeRate {
-    public ExchangeRate(Float rate) {
+    @Id
+    @GeneratedValue
+    private Long id;
 
+    @Column(name = "rate")
+    private float rate;
+
+    public ExchangeRate(float rate, Date timestamp) {
+        Assert.isTrue(rate > 0, "Rate must be greater than 0");
+
+        this.rate = rate;
     }
 }
--- src/test/java/ru/wiseman/onlinecurrencyconverter/domain/ExchangeRateEntityTest.java
+++ src/test/java/ru/wiseman/onlinecurrencyconverter/domain/ExchangeRateEntityTest.java
@@ -7,16 +7,27 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Date;
+
 @RunWith(SpringRunner.class)
 @DataJpaTest
 public class ExchangeRateEntityTest {
+    private static final Date TIMESTAMP = new Date();
+
     @Rule
     public ExpectedException thrown = ExpectedException.none();
 
     @Test
     public void createWhenRateIs0ShouldThrowException() throws Exception {
         thrown.expect(IllegalArgumentException.class);
-        thrown.expectMessage("Rate must be greater then 0");
-        new ExchangeRate(0f);
+        thrown.expectMessage("Rate must be greater than 0");
+        new ExchangeRate(0f, TIMESTAMP);
+    }
+
+    @Test
+    public void createWhenTimestampIsEmptyShouldThrowException() throws Exception {
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("Timestamp must not be empty");
+        new ExchangeRate(1f, null);
     }
 }
