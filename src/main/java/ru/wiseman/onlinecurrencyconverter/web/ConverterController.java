e864c528480402f034044b34a47b3e6c08150547
Mikhail Vaysman <Vaysman@users.noreply.github.com>
2017-11-19 16:14:00 +0100

    add ConverterController buy test

---
+++ src/main/java/ru/wiseman/onlinecurrencyconverter/web/ConverterController.java
@@ -0,0 +1,7 @@
+package ru.wiseman.onlinecurrencyconverter.web;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConverterController {
+}
---
+++ src/test/java/ru/wiseman/onlinecurrencyconverter/web/ConverterControllerTest.java
@@ -0,0 +1,28 @@
+package ru.wiseman.onlinecurrencyconverter.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(ConverterController.class)
+public class ConverterControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void buyWhenRequestSumShouldReturnNumberAndOk() throws Exception {
+        mvc.perform(get("/yconverter/buy?sum={}", "123.45").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().json("{sum: 67.89, status: 'OK'}"));
+    }
+}
