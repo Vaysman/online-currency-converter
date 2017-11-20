180baa500b7b932718c6947c2de1d8f7cbe8470e
Mikhail Vaysman <Vaysman@users.noreply.github.com>
2017-11-20 14:24:16 +0100

    add scheduling

--- src/main/java/ru/wiseman/onlinecurrencyconverter/OnlineCurrencyConverterApplication.java
+++ src/main/java/ru/wiseman/onlinecurrencyconverter/OnlineCurrencyConverterApplication.java
@@ -2,8 +2,10 @@ package ru.wiseman.onlinecurrencyconverter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class OnlineCurrencyConverterApplication {
 
     public static void main(String[] args) {
--- src/main/java/ru/wiseman/onlinecurrencyconverter/service/ExchangeRateUpdaterService.java
+++ src/main/java/ru/wiseman/onlinecurrencyconverter/service/ExchangeRateUpdaterService.java
@@ -3,6 +3,7 @@ package ru.wiseman.onlinecurrencyconverter.service;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import ru.wiseman.onlinecurrencyconverter.domain.ExchangeRate;
 
@@ -19,6 +20,7 @@ public class ExchangeRateUpdaterService {
     @NotNull
     private final RemoteExchangeRateService service;
 
+    @Scheduled(fixedRate = 60 * 60 * 1000)
     public void updateRate() {
         float rate = service.getRate();
         repository.save(new ExchangeRate(rate, new Date()));
