2dbd2d0451194e413e833803c784effe04240316
Mikhail Vaysman <Vaysman@users.noreply.github.com>
2017-11-20 01:12:27 +0100

    update OperationDetails

--- src/main/java/ru/wiseman/onlinecurrencyconverter/service/OperationDetails.java
+++ src/main/java/ru/wiseman/onlinecurrencyconverter/service/OperationDetails.java
@@ -6,4 +6,13 @@ import lombok.Value;
 public class OperationDetails {
     String status;
     Float sum;
+
+    public OperationDetails(String status, Float sum) {
+        this.status = status;
+        this.sum = truncate(sum);
+    }
+
+    private Float truncate(float sum) {
+        return (float) (Math.floor(sum * 100.0) / 100.0);
+    }
 }
