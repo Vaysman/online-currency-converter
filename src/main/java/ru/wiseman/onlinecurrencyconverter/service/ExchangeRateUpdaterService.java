4f6cdaf7efc5b841b0e83b715bef6c92d876b91f
Mikhail Vaysman <Vaysman@users.noreply.github.com>
2017-11-20 14:19:17 +0100

    implement ExchangeRateUpdaterService

--- src/main/java/ru/wiseman/onlinecurrencyconverter/service/ExchangeRateUpdaterService.java
+++ src/main/java/ru/wiseman/onlinecurrencyconverter/service/ExchangeRateUpdaterService.java
@@ -4,8 +4,10 @@ import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import ru.wiseman.onlinecurrencyconverter.domain.ExchangeRate;
 
 import javax.validation.constraints.NotNull;
+import java.util.Date;
 
 @Service
 @Slf4j
@@ -18,5 +20,7 @@ public class ExchangeRateUpdaterService {
     private final RemoteExchangeRateService service;
 
     public void updateRate() {
+        float rate = service.getRate();
+        repository.save(new ExchangeRate(rate, new Date()));
     }
 }
